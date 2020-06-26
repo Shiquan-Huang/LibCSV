2bb773bd0cc33bdefa864a631e33fea65b2ef4a7
hsq1596753614 <1357626165@qq.com>
2020-06-26 23:39:41 +0800

    definitions of functions

--- LibCSV/LibCSV.cpp
+++ LibCSV/LibCSV.cpp
@@ -1,7 +1,43 @@
 #include "pch.h"
 #include "LibCSV.h"
 
-
-LibCSV::LibCSV()
+int LibCSV::load_csv(const std::string& csv_path)
 {
+	return 0;
+}
+
+int LibCSV::row_count()
+{
+	return 0;
+}
+
+int LibCSV::col_count()
+{
+	return 0;
+}
+
+const string& LibCSV::get_element(const int& row, const int& col)
+{
+	// TODO: 在此处插入 return 语句
+	return "";
+}
+
+int LibCSV::add_row(const std::vector<std::string>& row_ctx)
+{
+	return 0;
+}
+
+int LibCSV::remove_row(const int& row)
+{
+	return 0;
+}
+
+int LibCSV::replace_row(const int& row, const std::vector<std::string>& row_ctx)
+{
+	return 0;
+}
+
+int LibCSV::update_element(const int& row, const int& col, const std::string& new_element)
+{
+	return 0;
 }
--- LibCSV/LibCSV.h
+++ LibCSV/LibCSV.h
@@ -3,14 +3,34 @@
 #else
 #define LIBCSV_API __declspec(dllimport)
 #endif // LIBCSV_EXPORTS
+#include <string>
+#include <vector>
+#include <list>
+#include <fstream>
 
+using namespace std;
 
 class LIBCSV_API LibCSV
 {
 public:
-	LibCSV(void);
+
+	int load_csv(const std::string& csv_path);
+
+	int row_count();
+
+	int col_count();
+
+	const string& get_element(const int& row, const int& col);
+
+	int add_row(const std::vector<std::string>& row_ctx);
+
+	int remove_row(const int& row);
+
+	int replace_row(const int& row, const std::vector<std::string>& row_ctx);
+
+	int update_element(const int& row, const int& col, const std::string& new_element);
 
 private:
-
+	std::ifstream csv_file;
 };
 
